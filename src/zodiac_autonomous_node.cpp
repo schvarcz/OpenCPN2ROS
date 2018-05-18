1e44cf47bee9e47acf196bd7f3932fcaeffb4f82
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2018-05-18 15:37:10 +0200

    [zodiac_msgs] Publish messages as Mael expect in zodiac.

--- src/zodiac_autonomous_node.cpp
+++ src/zodiac_autonomous_node.cpp
@@ -4,8 +4,19 @@
 #include <string>
 #include <ros/ros.h>
 #include <nmea_msgs/Sentence.h>
+#include <zodiac_command/WaypointListMission.h>
 
 using namespace std;
+
+float nmeaToDeg(float nmeaNro)
+{
+  return floor(nmeaNro/100.) + (nmeaNro/100.-floor(nmeaNro/100.))*100/60;
+}
+
+float degToNmea(float deg)
+{
+  return floor(deg)*100. + (deg - floor(deg))*60;
+}
 
 struct NMEA_WPL
 {
@@ -17,14 +28,18 @@ struct NMEA_WPL
     std::getline(parts,part,',');
 
     std::getline(parts,part,',');
-    this->x = std::stod(part);
+    this->latitude = nmeaToDeg(std::stod(part));
     std::getline(parts,part,',');
-    this->xH = part;
+    this->latitudeH = part;
+    if(this->latitudeH.compare("S") == 0)
+      this->latitude = -this->latitude;
 
     std::getline(parts,part,',');
-    this->y = std::stod(part);
+    this->longitude = nmeaToDeg(std::stod(part));
     std::getline(parts,part,',');
-    this->yH = part;
+    this->longitudeH = part;
+    if(this->longitudeH.compare("S") == 0)
+      this->longitude = -this->longitude;
 
     this->name = part;
   }
@@ -32,12 +47,12 @@ struct NMEA_WPL
   string toString()
   {
     std::stringstream ss;
-    ss << this->name << ": " << this->x << ", " << this->y;
+    ss << this->name << ": " << this->latitude << ", " << this->longitude;
     return ss.str();
   }
 
-  double x, y;
-  string xH, yH, name;
+  double latitude, longitude;
+  string latitudeH, longitudeH, name;
 };
 
 struct NMEA_RTE
@@ -87,6 +102,7 @@ public:
 
     sub1 = n.subscribe("/nmea_sentence", 1000000, &ZodiacAutonomous::nmeaSentenceCallback, this);
     pub1 = n.advertise<nmea_msgs::Sentence>("/nmea_sentence",0);
+    pub2 = n.advertise<zodiac_command::WaypointListMission>("/waypoints",0);
   }
 
   void nmeaSentenceCallback(const nmea_msgs::Sentence sentence_msg)
@@ -104,53 +120,75 @@ public:
       {
         loaded = false;
         pts.clear();
-        publishBoatPose(sentence_msg);
+//        publishBoatPose();
       }
       NMEA_WPL wp(sentence_msg.sentence);
       pts.push_back(wp);
-      cout << wp.toString() << endl;
     }
     else if(part.compare("$ECRTE") == 0)
     {
+      if(!loaded)
+        publishWaypointList();
       loaded = true;
       NMEA_RTE rtem(sentence_msg.sentence);
       rte.push_back(rtem);
-      cout << rtem.toString() << endl;
     }
   }
 
-  void publishBoatPose(nmea_msgs::Sentence old_msg)
+  void publishWaypointList()
   {
-    std::stringstream msg_ss, msg_final;
-    string msg_s;
-    //    msg_ss << "GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W";
-    msg_ss << "GPGGA,123519,4807.038,N,01131.000,E,1,10,0.9,545.4,M,46.9,M,,";
-//    msg_ss  << "GPRMC,123519,A," << 4807.038 + rand() %200 << ",N," << 1131.000 + rand() %200 << ",E,022.4,084.4,230394,003.1,W";
-    msg_s = msg_ss.str();
+    zodiac_command::WaypointListMission waypoint_msg;
+    waypoint_msg.header.stamp = ros::Time::now();
+    waypoint_msg.header.frame_id = "mission_waypoints";
+    waypoint_msg.child_frame_id = "map";
 
-    msg_final << "$" << msg_s << "*" << std::hex << (checksum(msg_s.c_str()) & 255);
-    nmea_msgs::Sentence msg = old_msg;
+    int i = 1;
+    for(auto pt : pts)
+    {
+      zodiac_command::WaypointMission wp;
+      wp.waypointID = i++;
+      wp.latitude  = pt.latitude;
+      wp.longitude = pt.longitude;
+      waypoint_msg.waypoints.push_back(wp);
+    }
+    pub2.publish(waypoint_msg);
+  }
+
+  void publishBoatPose()
+  {
+    nmea_msgs::Sentence msg;
     msg.header.stamp = ros::Time::now();
-    msg.sentence = msg_final.str();
+    msg.sentence = packMessage("GPGGA,123519,4807.038,N,01131.000,E,1,10,0.9,545.4,M,46.9,M,,");
     pub1.publish(msg);
+    msg.sentence = packMessage("GPGLL,4916.45,N,12311.12,W,225444,A,");
+    pub1.publish(msg);
+    msg.sentence = packMessage("GPGSA,A,3,04,05,,09,12,,,24,,,,,2.5,1.3,2.1");
+    pub1.publish(msg);
+    msg.sentence = packMessage("GPGSV,2,1,08,01,40,083,46,02,17,308,41,12,07,344,39,14,22,228,45");
+    pub1.publish(msg);
+  }
+
+  string packMessage(string message)
+  {
+    std::stringstream msg_final;
+    msg_final << "$" << message << "*" << std::hex << (checksum(message.c_str()) & 255);
+    return msg_final.str();
   }
 
   int checksum(const char *str)
   {
-    int i = 0;
-    int ret = 0;
-    while(str[i] != '\0')
-    {
-      ret ^= str[i];
-      i++;
-    }
+    int i = 0, ret = 0;
+
+    while(str[i])
+      ret ^= str[i++];
+
     return ret;
   }
 
 private:
   ros::NodeHandle n;
   ros::Subscriber sub1;
-  ros::Publisher pub1;
+  ros::Publisher pub1, pub2;
   vector<NMEA_WPL> pts;
   vector<NMEA_RTE> rte;
   bool loaded;
@@ -163,4 +201,11 @@ int main(int argc, char** argv)
   ZodiacAutonomous mZodiacAutonomous;
 
   ros::spin();
+
+//  ros::Rate rate(30);
+//  while(rate.sleep())
+//  {
+//    mZodiacAutonomous.publishBoatPose();
+//  }
+//  ros::spinOnce();
 }
